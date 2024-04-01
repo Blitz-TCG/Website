66452b06aada4035463043453743c29cecbe99fb
Mick Blackwell <mick_blackwell@outlook.com>
2024-04-01 18:44:19 -0400

    connector updates

--- src/app/wallet.service.ts
+++ src/app/wallet.service.ts
@@ -4,16 +4,40 @@ import { getDatabase, ref, child, get } from 'firebase/database'; // Adjust the 
 import { Observable, from } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, PLATFORM_ID } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
   private walletID: string | null = null;
+  private walletConnectedSource = new BehaviorSubject<boolean>(false);
+  private walletUpdatedSource = new BehaviorSubject<string | null>(null);
+  private resetTokenStateSource = new BehaviorSubject<boolean>(false);
+
+ walletConnected$ = this.walletConnectedSource.asObservable();
+ walletUpdated$ = this.walletUpdatedSource.asObservable();
 
   constructor(@Inject(PLATFORM_ID) private platformId: object) { }
 
-  // Get wallet address
+  resetTokenState() {
+    this.resetTokenStateSource.next(true);
+  }
+
+  setWalletConnected(status: boolean): void {
+    this.walletConnectedSource.next(status);
+    // Update localStorage whenever the state changes
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('userIsConnected', String(status));
+    }
+  }
+
+  updateWalletID(walletID: string | null) {
+    this.walletID = walletID;
+    this.walletUpdatedSource.next(walletID);
+  }
+
+  // Method to fetch wallet address (adjusted as needed for your application)
   getWalletAddress(): Observable<void> {
     return from(this.fetchWalletAddress());
   }
@@ -23,19 +47,18 @@ export class WalletService {
       const auth = getAuth();
       const user = auth.currentUser;
       const database = getDatabase();
-      const dbRef = ref(database);
 
       if (!user) return;
 
-      const snapshot = await get(child(dbRef, `users/${user.uid}/wallet`));
-
-      if (!snapshot.exists()) return;
-
-      this.walletID = snapshot.val() === 'none'
-        ? this.handleNoneValue()
-        : "snapshot.val()"; // Note: Direct assignment. Adjust if needed.
+      const snapshot = await get(child(ref(database), `users/${user.uid}/wallet`));
+      if (snapshot.exists()) {
+        const walletValue = snapshot.val();
+        this.walletID = walletValue === 'none' ? null : walletValue;
+        // Optionally, update the connected state based on the retrieved value
+        this.setWalletConnected(walletValue !== 'none');
+      }
     } catch (error) {
-      throw new Error(error as any);
+      console.error("Error fetching wallet address:", error);
     }
   }
 
